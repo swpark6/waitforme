72f19ff5e35bff0522b577a313627176e01bdfb6
박세원 <sewon1234@naver.com>
2019-09-13 13:18:40 +0000

    add login scaffold page

---
+++ pages/login.js
@@ -0,0 +1,22 @@
+<template>
+  <v-container fluid>
+    <v-layout 
+      row 
+      wrap
+    >
+      <v-card>
+        Login
+      </v-card>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  import countdown from 'countdown'
+  
+  export default {
+  }
+</script>
+
+<style scoped>
+</style>
